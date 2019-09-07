7d419c6b3c23f6bf928e75bd6b7cc3055df851f1
dufajun <2695647975@qq.com>
2019-09-07 16:17:53 +0800

    vue-ele-app

--- vue/vue-ele-app/src/App.vue
+++ vue/vue-ele-app/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -29,6 +40,29 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+@import './common/stylus/mixin.styl'
+  .tab
+    width 100%
+    height 40px
+    display flex
+    // align-items center
+    line-height 40px
+    border-bottom 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(7, 17, 27, 0.1))
+    &-item
+      flex 1
+      text-align center
+      & > a
+        display block
+        font-size 14px
+        text-decoration none
+        color rgb(77, 85, 93)
+
+        &.router-link-active
+          color rgb(240, 20, 20)
+
+
+
 
 </style>
