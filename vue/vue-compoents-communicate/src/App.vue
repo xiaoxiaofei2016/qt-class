0119917f6a5c73e8497aa7bf33fd70347321ce8e
dufajun <2695647975@qq.com>
2019-09-16 22:53:24 +0800

    components-comunicate

---
+++ vue/vue-compoents-communicate/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="child1">
+    <child1 :msg="msg" :msg2="msg2" :fn="appFn" v-bind="test" :count="count" @addCount="add1" :add="add1"></child1>
+  </div>
+</template>
+
+<script>
+import child1 from '@/components/common/child1'
+export default {
+  name: 'App',
+  components: {
+    child1
+  },
+  data () {
+    return {
+      msg: '给child1的礼物',
+      msg2: '再次给child1的礼物',
+      test: {
+        a: 1,
+        b: 2
+      },
+      count: 1
+    }
+  },
+  methods: {
+    appFn () {
+      console.log('我是app.vue的方法')
+    },
+    add1 () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue-compoents-communicate/src/components/common/child1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{msg}}
+    {{msg2}}
+    <button @click="fn">按钮</button>
+    {{a}}
+    <button @click="add">+</button>
+    {{count}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg','msg2','fn','a','count','add'], //props里的数据不能被修改,如果父组件用v-bind绑定一个字段，子组件可以拿到字段内的子属性
+  computed: {
+
+  },
+  methods: {
+    // add () {
+
+    //   // this.count++
+    //   this.$emit('addCount')
+    // }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
