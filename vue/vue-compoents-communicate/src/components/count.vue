a4dcccf2f86e3bdce8bc6282fedd3349bfbc9cb6
dufajun <2695647975@qq.com>
2019-09-20 18:48:49 +0800

    vue-compoents-communicate

--- vue/vue-compoents-communicate/src/components/count.vue
+++ vue/vue-compoents-communicate/src/components/count.vue
@@ -2,21 +2,48 @@
   <div class="count">
     <h2>{{msg}}</h2>
     <hr/>
-    <h3>{{$store.state.count}}</h3>
+    <h3>{{count}}</h3>
     <!-- 取仓库里的数据 -->
-    <button @click="$store.commit('add')">+</button>
+    <button @click="add(5)">+</button>
     <!-- 取用仓库里的方法 -->
     <button @click="$store.commit('reduce')">-</button>
+    <hr/>
+    <p>{{date}}</p>
+    <hr/>
+    <p>{{money}}</p>
+    <button @click="makeMoneyAction">挣钱</button>
   </div>
 </template>
 
 <script>
 import store from '@/store/index.js'
+import { mapState, mapMutations, mapGetters, mapActions } from  'vuex' // mapState,mapMutations是vuex自带的
 export default {
   data () {
     return {
       msg: 'hello'
     }
+  },
+  // computed: {
+  //   date () {
+  //     return this.$store.state.date
+  //   }
+  // },
+
+  computed: {
+    ...mapState(['date', 'money']), // 解构
+    ...mapGetters(['count']) //把gettters方法直接拿页面上用
+    // count () {
+    //   return this.$store.getters.count
+    // }
+  }, // date直接放页面上用
+
+  // computed: mapState({
+  //   date: state => state.date
+  // }),
+  methods: {
+    ...mapMutations(['add']),
+    ...mapActions(['makeMoneyAction'])
   },
   store
 }
--- vue/vue-compoents-communicate/src/store/index.js
+++ vue/vue-compoents-communicate/src/store/index.js
@@ -4,19 +4,41 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  count:1 
+  count: 1,
+  date: '2019.9.20',
+  money: 100
 }
 
-const mutations = {
-  add (state) {
-    state.count++
+const mutations = { // 同步
+  add (state, n) {
+    state.count += n
   },
-  reduce () {
+  reduce (state) {
     state.count--
+  },
+  makeMoney (state) {
+    state.money += 1
   }
 }
 
-export default new Vuex.Store({
+const actions = { //异步
+  makeMoneyAction ({commit}) {
+    setTimeout(() => {
+      commit('makeMoney') // 使用action提交makeMoney方法，异步
+    },2000)
+    console.log('挣钱')
+  }
+}
+
+const getters = { // vuex计算属性
+  count: function(state) {
+    return state.count += 100
+  }
+}
+
+export default new Vuex.Store({ // 抛出方法和数据
   state,
-  mutations
+  mutations,
+  getters,
+  actions
 })
