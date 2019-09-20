6b8503292210cdfbca067933422328cc5f03593a
dufajun <2695647975@qq.com>
2019-09-20 17:03:58 +0800

    vue-cloud-music

---
+++ vue/vue-cloud-music/src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import def_lazy_img from '../static/img/loading.gif'
+
+Vue.use(VueLazyload, {
+  loading: def_lazy_img
+})
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
