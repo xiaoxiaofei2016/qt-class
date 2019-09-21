bd3236f861145e3f8547610cf9af3f076f7b1f24
dufajun <2695647975@qq.com>
2019-09-21 20:25:44 +0800

    vue-menu

---
+++ vue/vue-menu/README.md
@@ -0,0 +1,10 @@
+- <slot></slot> 插槽
+  <Menu>
+  
+  </Menu>
+- 像设计盒子一样设计组件
+ <Menu></Menu> 容器组件
+ 只有html，css   
+ style-component
+- template 是高效设计的一个法宝
+  承载指令 用的舒服
---
+++ vue/vue-menu/src/components/MenuList.vue
@@ -0,0 +1,39 @@
+<template>
+<!-- 容器组件 -->
+  <Menu>
+    <template v-for="menu in data">
+      <MenuItem :key="menu.id" 
+      v-if="!menu.children">
+      {{menu.title}}
+      </MenuItem>
+      <ReSubMenu :key="menu.id" 
+      :data="menu"
+      v-else>
+      
+      </ReSubMenu>
+    </template>
+  </Menu>
+</template>
+
+<script>
+import Menu from './Menu'
+import MenuItem from './MenuItem'
+import ReSubMenu from './ReSubMenu'
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => ([])
+    }
+  },
+  components: {
+    Menu,
+    MenuItem,
+    ReSubMenu
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
