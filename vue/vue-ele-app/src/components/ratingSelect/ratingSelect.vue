0096e94db28685772a611c6a0fac31a6cf6d40cf
dufajun <2695647975@qq.com>
2019-09-21 12:03:09 +0800

    vue-ele-app

---
+++ vue/vue-ele-app/src/components/ratingSelect/ratingSelect.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <div class="ratingselect">
+        <div class="rating-type">
+          <div class="block positive" :class="{'active': isActive === 2}" @click="all(2)">
+            {{desc.all}}
+            <span class="count">{{seller.ratingCount}}</span>
+          </div>
+          <div class="block positive" :class="{'active': isActive === 0}" @click="filterClick(0)"> 
+            {{desc.positive}}
+            <span class="count">{{goodscore}}</span>
+          </div>
+          <div class="block negative" :class="{'active': isActive === 1}" @click="filterClick(1)">
+            {{desc.negative}}
+            <span class="count">{{seller.ratingCount-goodscore}}</span>
+          </div>
+        </div>
+        <div class="switch on" @click="content">
+          <span class="icon-check_circle" :class="{'highlight': option}"></span>
+          <span class="text">只看有内容的评价</span>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ratings: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    seller: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      desc: {
+        all: '全部',
+        positive: '满意',
+        negative: '不满意'
+      },
+      option: false,
+      isActive: 2,
+      goodscore: 0,
+      displayComm: []
+    }
+  },
+  methods: {
+    goodScore () {
+      for (let i = 0;i < this.ratings.length; i++) {
+        if (this.ratings[i].score >=4 || this.ratings[i].rateType == 0) {
+          this.goodscore++
+        }
+      }
+      console.log(this.goodscore) // 18
+      return this.goodscore
+    },
+    
+    all (num) {
+      this.displayComm = this.ratings 
+      this.isActive = num // 通过改变isActive的值，从而实现类名active的添加或删除
+    },
+    filterClick (condition) {
+      let retArr = []
+      for (let rating of this.ratings) {
+        if (condition === rating.rateType) {
+          retArr.push(rating)
+        }
+      }
+      this.displayComm = retArr
+      this.isActive = condition // 通过改变condition的值改变isActive的值，从而实现类名active的添加或删除
+    },
+    content () {
+      let retArr = []
+      for (let rating of this.displayComm) {
+        if (rating.text && rating.text !== '') {
+          retArr.push(rating)
+        }
+      }
+      this.option = !this.option
+      if (this.option) {
+        this.displayComm = retArr
+      }else {
+        this.displayComm = this.ratings
+      }
+    },
+    data () {
+      this.$emit('displayComm', this.displayComm)
+    }
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+@import "../../common/stylus/mixin.styl"
+.ratingselect
+      width 100%
+      .rating-type
+        padding 18px 0
+        margin 0 18px
+        position relative
+        font-size 0
+        border-bottom 1px solid rgba(7,17,27,.1)
+        .block
+          display inline-block
+          margin-right 8px
+          padding 8px 12px
+          font-size 12px
+          line-height 16px
+          border-radius 1px
+          color #4D555D
+          .count
+            display inline-block
+            margin-left 2px
+            font-size 8px
+        .all
+          background #CCECF8
+        .positive
+          background #CCECF8
+        .negative
+          background #DBDDDF
+        .all.active
+          background #00a0dc
+          color #ffffff
+        .positive.active
+          background #00a0dc
+          color #ffffff
+        .negative.active
+          background #4D555D
+          color #ffffff
+      .switch
+        padding 12px 18px
+        line-height 24px
+        border-bottom 1px solid rgba(7,17,27,.1)    
+        color #93999f
+        font-size 0
+        .icon-check_circle
+          color #93999f
+          display inline-block
+          vertical-align top
+          margin-right 4px
+          font-size 24px
+        .icon-check_circle.highlight
+          color #00c850
+        .text
+          display inline-block
+          vertical-align top
+          font-size 12px
+</style>
--- vue/vue-ele-app/src/components/ratings/ratings.vue
+++ vue/vue-ele-app/src/components/ratings/ratings.vue
@@ -25,29 +25,10 @@
         </div>
       </div>
       <div class="split"></div>
-      <div class="ratingselect">
-        <div class="rating-type">
-          <div class="block positive" :class="{'active': isActive === 2}" @click.stop.prevent="all(2)">
-            全部
-            <span class="count">{{seller.ratingCount}}</span>
-          </div>
-          <div class="block positive" :class="{'active': isActive === 0}" @click.stop.prevent="filterClick(0)"> 
-            满意
-            <span class="count">{{goodscore}}</span>
-          </div>
-          <div class="block negative" :class="{'active': isActive === 1}" @click.stop.prevent="filterClick(1)">
-            不满意
-            <span class="count">{{seller.ratingCount-goodscore}}</span>
-          </div>
-        </div>
-        <div class="switch on" @click="content">
-          <span class="icon-check_circle" :class="{'highlight': option}"></span>
-          <span class="text">只看有内容的评价</span>
-        </div>
-      </div>
+      <RatingSelect :ratings="ratings" :isActive="isActive" :option="option" :seller="seller"></RatingSelect>
       <div class="rating-wrapper" >
         <ul>
-          <li class="rating-item" v-for="(item, index) in displayComments" :key="index">
+          <li class="rating-item" @displayComm="data" v-for="(item, index) in displayComms" :key="index">
             <div class="avatar">
               <img :src="item.avatar" alt="" width="28" height="28">
             </div>
@@ -74,9 +55,11 @@
 <script>
 import BScroll from 'better-scroll'
 import star from '@/components/star/star'
+import RatingSelect from '@/components/ratingSelect/ratingSelect'
 export default {
   components: {
-    star
+    star,
+    RatingSelect
   },
   props: {
     seller: {
@@ -86,26 +69,19 @@ export default {
   data () {
     return {
       ratings: [],
-      goodscore: 0,
-      displayComm: [],
+      displayComms: [],
       option: false,
       isActive: 2
     }
-  },
-  computed: {
-    displayComments () {
-      return this.displayComm
-    }
-  },
+  },  
   created () {
     this.$http.get('http://localhost:8080/static/ratings.json')
     .then(res => {
       console.log(res)
       if (res.data.errno === 0) {
         this.ratings = res.data.data
-        this.displayComm = this.ratings
+        // this.displayComm = this.ratings
         this.$nextTick(() => {
-          this.goodScore()
           this.initScroll()
         })
       }
@@ -127,44 +103,10 @@ export default {
       this.commScroll = new BScroll(this.$refs.ratings, {
         click: true
       })
-      console.log(this.commScroll)
+      // console.log(this.commScroll)
     },
-    goodScore () {
-      for (let i = 0;i < this.ratings.length; i++) {
-        if (this.ratings[i].score >=4 || this.ratings[i].rateType == 0) {
-          this.goodscore++
-        }
-      }
-      console.log(this.goodscore) // 18
-      return this.goodscore
-    },
-    all (num) {
-      this.displayComm = this.ratings 
-      this.isActive = num // 通过改变isActive的值，从而实现类名active的添加或删除
-    },
-    filterClick (condition) {
-      let retArr = []
-      for (let rating of this.ratings) {
-        if (condition === rating.rateType) {
-          retArr.push(rating)
-        }
-      }
-      this.displayComm = retArr
-      this.isActive = condition // 通过改变condition的值改变isActive的值，从而实现类名active的添加或删除
-    },
-    content () {
-      let retArr = []
-      for (let rating of this.displayComm) {
-        if (rating.text && rating.text !== '') {
-          retArr.push(rating)
-        }
-      }
-      this.option = !this.option
-      if (this.option) {
-        this.displayComm = retArr
-      }else {
-        this.displayComm = this.ratings
-      }
+    data (displayComm) {
+      this.displayComms = displayComm
     }
   }
 }
@@ -239,59 +181,7 @@ export default {
       border-top 1px solid rgba(7,17,27,.1)
       border-bottom 1px solid rgba(7,17,27,.1)
       background #f3f5f7
-    .ratingselect
-      width 100%
-      .rating-type
-        padding 18px 0
-        margin 0 18px
-        position relative
-        font-size 0
-        border-bottom 1px solid rgba(7,17,27,.1)
-        .block
-          display inline-block
-          margin-right 8px
-          padding 8px 12px
-          font-size 12px
-          line-height 16px
-          border-radius 1px
-          color #4D555D
-          .count
-            display inline-block
-            margin-left 2px
-            font-size 8px
-        .all
-          background #CCECF8
-        .positive
-          background #CCECF8
-        .negative
-          background #DBDDDF
-        .all.active
-          background #00a0dc
-          color #ffffff
-        .positive.active
-          background #00a0dc
-          color #ffffff
-        .negative.active
-          background #4D555D
-          color #ffffff
-      .switch
-        padding 12px 18px
-        line-height 24px
-        border-bottom 1px solid rgba(7,17,27,.1)    
-        color #93999f
-        font-size 0
-        .icon-check_circle
-          color #93999f
-          display inline-block
-          vertical-align top
-          margin-right 4px
-          font-size 24px
-        .icon-check_circle.highlight
-          color #00c850
-        .text
-          display inline-block
-          vertical-align top
-          font-size 12px
+    
 
     .rating-wrapper
       padding 0 18px
