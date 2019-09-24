3b40dea9fb5ce82a08f7c36561335b47a364646e
dufajun <2695647975@qq.com>
2019-09-24 18:25:19 +0800

    workflow

---
+++ workflow/dfj-cli/src/main.js
@@ -0,0 +1,12 @@
+import program from 'commander'; //安装 命令行的处理
+import install from './install'; // es6 模块化
+
+program.command('install')
+  .description('install template')
+  .alias('i') // 短链接
+  .action(() => {
+    install();
+  })
+console.log('program');
+program.parse(process.argv);
+
