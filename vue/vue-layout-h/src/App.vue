72bb2d3a7060937d78d6d87fe96387ed1df60521
dufajun <2695647975@qq.com>
2019-09-27 20:44:49 +0800

    layout-h

---
+++ vue/vue-layout-h/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <el-button :plain="true" @click="openVn">VNode</el-button>
+    <my-elrow tag="div">
+      列
+    </my-elrow>
+  </div>
+</template>
+
+<script>
+import ElRow from './components/El-Row'
+export default {
+  name: 'App',
+  components: {
+    'my-elrow': ElRow
+  },
+  methods: {
+    openVn() {
+      const h = this.$createElement;
+      this.$message({
+        message: h('p', null, [
+          h('span', null, '内容可以是 '),
+          h('i', { style: 'color: teal' }, 'VNode')
+        ]),
+        duration: 0 // 弹出不消失
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-layout-h/src/components/El-Row.vue
@@ -0,0 +1,23 @@
+
+<script>
+export default {
+// <template>
+//  <div class="el-row">
+//   <slot></slot>
+//  </div>
+// </template>
+
+  render(h) {
+    // h === createElement
+    return h(
+      this.tag,
+      {},
+      []
+    ) 
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
