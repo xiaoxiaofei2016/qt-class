be955b16f1b7613f53ca47a0cad29c66370903bd
dufajun <2695647975@qq.com>
2019-09-28 16:16:07 +0800

    vue-hover-zoom-image

--- vue/good-hover-zoom-image/src/components/Child.vue
+++ vue/good-hover-zoom-image/src/components/Child.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    
+    <subChild></subChild>
   </div>
 </template>
 
 <script>
+import subChild from './SubChild'
 export default {
+  components: {
+    subChild
+  }
 }
 </script>
 
