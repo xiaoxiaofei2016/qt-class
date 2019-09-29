8de0d7b6b05feee500a126af2ca6adb33d57c629
dufajun <2695647975@qq.com>
2019-09-29 20:23:25 +0800

    data

---
+++ vue/vue-data-visualization-profile/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// 引入echarts
+import echarts from 'echarts'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+Vue.prototype.$echarts = echarts
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
