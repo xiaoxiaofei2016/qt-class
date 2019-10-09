db71ab4515c5d74bda30d69ae3b5a430df536920
dufajun <2695647975@qq.com>
2019-10-09 20:11:02 +0800

    toutiao

--- vue/vue-toutiao/src/components/HeaderBar.vue
+++ vue/vue-toutiao/src/components/HeaderBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header-wrapper df-sb">
     <div class="logo df-c">
-      <img src="../../assets/images/logo.png" alt="LOGO" />
+      <img src="../assets/logo.png" alt="LOGO" />
     </div>
     <div class="search">
       <Icon name="2fangdajing" class="search-icon"></Icon>
