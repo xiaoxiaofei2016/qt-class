92a8f6c25917346f7d5c4dfd2e05091c2d6cb344
dufajun <2695647975@qq.com>
2019-10-11 16:37:21 +0800

    directive

--- vue/vue-directive/index.js
+++ vue/vue-directive/index.js
@@ -1,7 +1,39 @@
 
 // <div v-debounce="open">立即购买</div>
-Vue.directive('debounce', {
+
+function debounce(fn, wait) {
+  var timeout = null;
+  return function () {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(fn, wait);
+  }
+}
+
+// Vue.directive('debounce', )
+let option = {
   inserted: (el, binding) => {
     console.log(el, binding)
-  }
-})
+  },
+  bind: function(el, binding) {
+    // onclick addEventListener
+    el.addEventListener('click', debounce(binding.value, 2000))
+    // el.addEventListener('click', () => {
+    //   el.style = "color" + binding.value
+    // })
+  },
+  unbind: function() {
+
+  },
+}
+
+let plugin = {}
+plugin.install = function(argVue) {
+  argVue.directive('debounce', option)
+}
+if (typeof exports === 'object') {
+  module.exports = plugin
+} else if (window) {
+  window.Vue.use
+}
