5fbf71c876a22649d4493cf604deb46015bd779b
dufajun <2695647975@qq.com>
2019-10-11 21:36:30 +0800

    node-fs

---
+++ node/node-fs/server.js
@@ -0,0 +1,32 @@
+let http = require('http') // 引入http这个核心功能块，来创建web服务器
+let fs = require('fs') // fs操作服务器的资源文件
+
+let server = http.createServer()
+server.on('request', (req, res) => {
+  let url = req.url
+  if (url === '/') {
+    fs.readFile('./resource/resource/index.html', (error, data) => {
+      if (error) {
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+        res.end('亲，你要的资源不存在，请稍后再试！')
+      } else {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end(data)
+      }
+    })
+  } else if (url === '/img') {
+      fs.readFile('./resource/resource/hy.jpg', (error, data) => {
+        if (error) {
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+          res.end('亲，你要的资源不存在，请稍后再试！')
+        } else {
+          res.setHeader('Content-Type', 'image/jpeg')
+          res.end(data)
+        }
+      })
+  }
+})
+
+server.listen(3000, () => {
+  console.log('服务器已启动，可以访问了')
+})
