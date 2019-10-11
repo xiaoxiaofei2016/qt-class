6348ea530e5ef971af754993f8664132a7b5489d
dufajun <2695647975@qq.com>
2019-10-11 15:32:12 +0800

    mixin

---
+++ vue/vue-mixin/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <my-dialog/>
+    <my-loading/>
+  </div>
+</template>
+
+<script>
+import dialog from '@/components/dialog'
+import loading from '@/components/loading'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    'my-dialog': dialog,
+    'my-loading': loading
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/vue-mixin/src/components/visible.js
@@ -0,0 +1,17 @@
+let mixin = {
+  data () {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    open() {
+      this.dialogVisible = true
+    },
+    close() {
+      this.dialogVisible = false
+    }
+  }
+}
+
+export default mixin
