c50ec3504fae9344e7c34767326fdfd873cae94f
dufajun <2695647975@qq.com>
2019-10-14 21:06:57 +0800

    components

---
+++ vue/vue-components/src/components/dispaly.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="display">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    code: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      html: '',
+      css: '',
+      js: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
