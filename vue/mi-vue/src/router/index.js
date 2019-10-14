8b72e25e438480cf0046e1c15a0e24aa94fcce1e
dufajun <2695647975@qq.com>
2019-10-14 11:15:17 +0800

    小米商城

---
+++ vue/mi-vue/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+const __import__ = file => () => import(`@/pages/${file}.vue`)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: __import__('home')
+    }
+  ]
+})
