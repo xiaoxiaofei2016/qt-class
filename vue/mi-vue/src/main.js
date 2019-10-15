d4001969b7a37180d3a655c6391b282e813f664f
dufajun <2695647975@qq.com>
2019-10-15 10:53:53 +0800

    小米商城

--- vue/mi-vue/src/main.js
+++ vue/mi-vue/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import './style/css/reset.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
