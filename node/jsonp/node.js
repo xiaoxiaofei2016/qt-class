53fa46baf496d63a7fbdf0d060725673778ab43e
dufajun <2695647975@qq.com>
2019-10-24 15:44:12 +0800

    jsonp

--- node/jsonp/node.js
+++ node/jsonp/node.js
@@ -1,9 +1,12 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  if (req.url === '/api/books') {
+  const url = require('url').parse(req.url)
+  if (url.pathname === '/api/books') {
+    const methodName = url.query && url.query.split('=')[1];    
+    console.log(methodName)
     let list = [{book: 'book2'}]
-    res.end(`getBook(${JSON.stringify(list)})`);
+    res.end(`${methodName}(${JSON.stringify(list)})`);
   }
 })
 .listen(3000, () => {
