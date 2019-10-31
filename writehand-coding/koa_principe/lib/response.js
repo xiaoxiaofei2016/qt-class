a9a6ededd2a27b23f723f0e15e8877a3243c0488
dufajun <2695647975@qq.com>
2019-10-31 22:43:20 +0800

    webpack

--- writehand-coding/koa_principe/lib/response.js
+++ writehand-coding/koa_principe/lib/response.js
@@ -1,3 +1,11 @@
-let response = {}
+let response = {
+  get body () {
+    return this._body // get时返回该方法
+  },
+  set body (value) {
+    this.res.statusCode = 200
+    this._body = value // set时先保存该方法
+  }
+}
 
 module.exports = response
