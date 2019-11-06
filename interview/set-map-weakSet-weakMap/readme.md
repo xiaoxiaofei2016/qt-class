8057f3f52fe13ef7c770b8f20a3aa9d8ddba7977
dufajun <2695647975@qq.com>
2019-11-06 21:07:59 +0800

    set-map

---
+++ interview/set-map-weakSet-weakMap/readme.md
@@ -0,0 +1,9 @@
+## Set 和 Weakset 区别
+1. WeakSet 只能存放对象
+2. Weakset 不支持遍历，没有size
+3. Weakset 存放的对象不会计入到对象的引用技术，因此不会影响GC的回收
+4. Weakset 存放的对象如果在外界消失了，那么Weakset里面也不会存在
+
+## Map 和 Weakmap 区别
+1. Weakmap 只能接受对象作为健名 (null除外)
+2. Weakmap 键名指向对象不会计入到对象的引用数当中
--- interview/set-map-weakSet-weakMap/weakSet.js
+++ interview/set-map-weakSet-weakMap/weakSet.js
@@ -1,1 +1,28 @@
-// weakSet 对象允许你将弱引用对象存储在一个集合中
+// weakSet 对象允许你将弱引用对象存储在一个集合中 --- 弱引用版本的Set
+// var test = {
+//   name: 'test',
+//   content: {
+//     name: 'wn',
+//     age: '12'
+//   }
+// }
+// var ws = new WeakSet()
+// ws.add(test.content)
+// var a = { foo: 'bar' }
+// ws.add(a)
+// ws.delete(a)
+// console.log(ws)
+// ws.delete(test.content) // delete前已经被回收了
+// console.log(ws)
+
+const foos = new WeakSet()
+class Foo {
+  constructor() {
+    foos.add(this)
+  }
+  method () {
+    if (!foos.has(this)) {
+      throw new TypeError('Foo.prototype.method只能在Foo的实例上调用')
+    }
+  }
+}
