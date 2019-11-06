39c4761d948234a2ec22f0ba13af6240baa43005
dufajun <2695647975@qq.com>
2019-11-06 15:54:54 +0800

    process-protect

---
+++ node/process-protect/netServer.js
@@ -0,0 +1,17 @@
+const net = require('net')
+const http = require('http')
+const server = net.createServer()
+const httpServer = http.createServer((req, res) => {
+  res.end('I am from http')
+  throw new Error('error')
+})
+server.on('connection', (socket) => {
+  // socket.on('data', (data) => {
+  //   console.log('server rec', data)
+  //   socket.write('hi')
+  // })
+  httpServer.emit('connection', socket)
+})
+server.listen(8181, () => {
+  console.log('server is running')
+})
