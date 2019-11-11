176faf9b5e1eb95335a2ad1aaa7b9ebe959f8a9b
dufajun <2695647975@qq.com>
2019-11-11 22:07:41 +0800

    xiaomitest

--- 爬数据/xiaomitest/index.js
+++ 爬数据/xiaomitest/index.js
@@ -29,5 +29,5 @@ app.get('/index', function(req, res) {
   })
 })
 var server = app.listen(3000, () => {
-  console.log('Server is running 3000')
+  console.log('Server is running http://localhost:3000')
 })
