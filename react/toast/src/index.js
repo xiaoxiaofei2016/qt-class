81a102079fd97af1cb25cac00eca72578f02d6bd
dufajun <2695647975@qq.com>
2019-11-13 21:33:32 +0800

    toast

--- react/toast/src/index.js
+++ react/toast/src/index.js
@@ -3,8 +3,53 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// react提倡不可变数据
+// 生成默认值
+// 后续 dispath -> action - reducer -store
+function discuss(state, action) {
+  switch (action.type) {
+    case 'PUSH_DIS':
+      return [
+        ...state,
+        action.content
+      ]
+    default:
+      return []
+  }
+  // return []
+}
+const store = createStore(discuss)
+setInterval(() => {
+  store.dispatch({
+    type: 'PUSH_DIS',
+    content: '评论1'
+  })
+}, 1000)
+class List extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          store.getState().map((dis) => {
+            return (
+              <li>{ dis }</li>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+store.subscribe(() => {
+  ReactDOM.render(<List />, document.getElementById('root'));
+})
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<List />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
