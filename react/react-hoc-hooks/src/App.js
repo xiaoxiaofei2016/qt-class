c1288350b03532d0c73d117e6c5b4981e6624218
dufajun <2695647975@qq.com>
2019-11-20 21:28:40 +0800

    hoc-hooks

--- react/react-hoc-hooks/src/App.js
+++ react/react-hoc-hooks/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import Drag1 from './Drag1.jsx'
 import './App.css';
 import withDragAble from './withDragAble.jsx'
+import useDrag from './useDragable'
 
 function Header() {
   return (
@@ -12,8 +13,9 @@ function Header() {
 function Footer() {
   // state hooks
   const [ count, setCount ] = useState(1) // state={ 0 } 0默认值
+  const { style, handleDown } = useDrag()
   return (
-    <div>
+    <div style={style} onMouseDown={handleDown} className="drag">
       foot: { count }
       <button onClick={() => {
         let count1 = count + 1
--- react/react-hoc-hooks/src/useDragable.jsx
+++ react/react-hoc-hooks/src/useDragable.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from 'react'
 
+// hooks
+// 自定义 hooks 方法名以use开头
 function useDragable() {
   let [ left, setLeft ] = useState(0)
   let [ top, setTop ] = useState(0)
-
+  let startx = 0,
+      starty = 0
+  const handleMouseMove = function(e) {
+    let left = e.clientX
+    let top = e.clientY
+    setLeft(left) // 修改left.value
+    setTop(top) // 修改top.value
+  }
+  const handleMouseUp = function() {
+    document.removeEventListener('mousemove', handleMouseMove)
+  }
+  const handleDown = function() {
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+  }
   return {
     style: {
       left,
       top
-    }
+    },
+    handleDown
   }
- 
 }
+
+export default useDragable
