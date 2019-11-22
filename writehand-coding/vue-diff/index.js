d98d138d42bacb70f48a9e9e14800f433926fe32
dufajun <2695647975@qq.com>
2019-11-22 16:33:13 +0800

    diff

--- writehand-coding/vue-diff/index.js
+++ writehand-coding/vue-diff/index.js
@@ -28,3 +28,37 @@ Element.prototype.render = function () {
 }
 
 // diff算法 对比两棵Dom树
+function diff (oldTree, newTree) {
+  let index = 0 // 节点的标志
+  let patches = {} // 记录每个节点差异的地方
+  dfsWalk(oldTree, newTree, index, patches) // 找不同
+  return patches
+}
+function dfsWalk(oldNode, newNode, index, patches) {
+  // 对比oldTree和newTree的差异，把不同的地方记录下来
+  var currentPatch = patches[walker.index]
+  if (newNode !== null && oldNode.tagName == newNode,tagName && oldNode.key !== newNode.key) {
+    currentPatch.push({ type: 'div', props: { id: 'container' } })
+  }
+  patches[index] = currentPatch
+
+  diffChildren(oldNode.children, newNode.children, index, patches)
+}
+
+function diffChildren(oldChildren, newChildren, index, patches) {
+  let leftNode = null
+  let currentNodeIndex = index
+  oldChildren.forEach((child, i) => {
+    var newChild = newChildren[i]
+    currentNodeIndex = (leftNode && leftNode.count) // 计算节点的标记
+    ? currentNodeIndex + leftNode.count + 1
+    : currentNodeIndex + 1
+    dfsWalk(child, newChild, currentNodeIndex, patches) // 遍历子节点，找子节点中的不同
+    leftNode = child
+  })
+}
+
+function patch(node, patches) {
+  var walker = { index: 0 } // 记录当前节点的标志
+  dfsWalk(node, walker, patches)
+}
