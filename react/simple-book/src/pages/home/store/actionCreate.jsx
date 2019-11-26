4eff398cb38360e97c5e7a8cd5ff97afa7fef89f
dufajun <2695647975@qq.com>
2019-11-26 20:53:04 +0800

    simple-book

--- react/simple-book/src/pages/home/store/actionCreate.jsx
+++ react/simple-book/src/pages/home/store/actionCreate.jsx
@@ -3,7 +3,7 @@ import { fromJS } from 'immutable';
 import * as constants from './constant';
 
 // redux-thunk 里面的action
-export const getHomeInfo = () => {
+export const getHomeInfo = () => { // 设置数据
   return (dispatch) => {
     axios.get('/api/home.json').then(res => {
       const result = res.data.data
