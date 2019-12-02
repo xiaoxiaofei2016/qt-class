edafed062a45ebdd050a5bb93dd538ef23ba8e56
dufajun <2695647975@qq.com>
2019-12-02 22:57:54 +0800

    blog

---
+++ vue3/blog/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Nav/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+// import Component from 'vue-class-component';
+import Nav from '@/components/nav.vue';
+@Component({
+  components: {
+    Nav
+  },
+})
+export default class App extends Vue {
+  
+}
+</script>
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue3/blog/src/components/RegisterAndLogin.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-dialog title="登录" :visible.sync="dialogVisible" @close="cancel">
+    <el-form label-width="80px">
+      <el-form-item label="邮箱">
+        <el-input v-model="params.email"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="params.password"></el-input>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
+@Component
+export default class RegisterAndLogin extends Vue {
+  @Prop({ default: false }) visible!: boolean;
+
+  private params: any = {
+    email: '',
+    password: ''
+  }
+  get dialogVisible(): boolean {
+    // 计算属性
+    return this.visible;
+  }
+  @Emit() // 往父组件抛出方法,返回值作为参数
+  private cancel(): boolean {
+    return false;
+  }
+}
+</script>
+
+<style scoped>
+</style>
