f95343cd989d33ffea470a2e251fa440c3fbd97c
dufajun <2695647975@qq.com>
2019-12-05 17:45:26 +0800

    blog

--- vue3/blog/src/App.vue
+++ vue3/blog/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <Nav v-if="isShowNav"/>
-    <router-view/>
+    <div class="layout">
+      <router-view/>
+      <Slider class="slider"/>
+    </div>
   </div>
 </template>
 
@@ -9,16 +12,23 @@
 import { Component, Vue, Watch } from 'vue-property-decorator';
 // import Component from 'vue-class-component';
 import Nav from '@/components/nav.vue';
+import Slider from '@/components/Slider.vue';
+
 @Component({
   components: {
-    Nav
+    Nav,
+    Slider
   },
 })
 export default class App extends Vue {
   private isShowNav: boolean = false;
   @Watch('$route')
   routeChange (val, oldVal) {
-    
+    if (val.path === '/') {
+      this.isShowNav = false;
+    } else {
+      this.isShowNav = true;
+    }
   }
 }
 </script>
@@ -29,6 +39,13 @@ export default class App extends Vue {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.layout {
+  display: flex;
+  justify-content: space-around;
+}
+.slider {
+  flex: 0 0 300px;
 }
 
 #nav {
--- vue3/blog/src/router/index.ts
+++ vue3/blog/src/router/index.ts
@@ -16,8 +16,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'), // 路由的按需加载
   },
+  {
+    path: '/articles',
+    name: 'articles',
+    component: () => import('../views/articles.vue')
+  }
 ];
 
 const router = new VueRouter({
