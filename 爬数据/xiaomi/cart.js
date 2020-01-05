54474fcaa0c1382a6596329fc3c1c775f1d311c8
dufajun <2695647975@qq.com>
2020-01-05 15:00:01 +0800

    爬数据

--- 爬数据/xiaomi/cart.js
+++ 爬数据/xiaomi/cart.js
@@ -6,7 +6,7 @@ var newsList = [] // 用来存新闻数据
 
 nightmare
 .goto('https://static.mi.com/cart/')
-.wait(9000) // 6000 的意思是当模拟浏览器打开后会等待6秒再爬取页面数据，有这段时间就可以做你想要的动态加载操作
+.wait(4000) // 6000 的意思是当模拟浏览器打开后会等待6秒再爬取页面数据，有这段时间就可以做你想要的动态加载操作
 .evaluate(() => document.querySelector("div.xm-recommend").innerHTML) // 这里一定要取到你想要爬取数据区域的容器的类名
 .then(htmlStr => {
   navList = getnavList(htmlStr) // getnavList() 函数将执行对原始数据的选择和处理
@@ -29,9 +29,12 @@ let getnavList  = (res) => {
 
       $('dl', this).each(function(index, item) {
         let navList = {
-          img: $(this).children("[width='160']").attr('src'), // attr('属性') 可以取到对应属性的值，包括行内样式的值
-          title: $(this).text(), // text() 取到文本内容
-          price: $(this).text()
+          img: $(this).children('dt').children('a').children('img').attr('src'), // attr('属性') 可以取到对应属性的值，包括行内样式的值
+          title: $(this).children('dd.xm-recommend-name').children('a').text(), // text() 取到文本内容
+          price: $(this).children('dd.xm-recommend-price').text(),
+          recommend: $(this).children('dd.xm-recommend-tips').contents().filter(function (index, content) {
+            return content.nodeType === 3
+          }).text()
         }
         newsList.push(navList)
       })
