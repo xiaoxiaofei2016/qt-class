644cd366047d41a877e2b42b223a081beb3aaf01
dufajun <2695647975@qq.com>
2020-04-28 19:36:11 +0800

    webpack学习

--- interview/webpack/demo_react/src/App.js
+++ interview/webpack/demo_react/src/App.js
@@ -5,9 +5,9 @@ import { add } from './utils/add'
 class App extends React.Component {
   render () {
     return (
-      <div>
-       <div className="hello" onClick={add}>你好啊</div>
-      </div>
+      <>
+       <div className="hello" onClick={add(1, 2)}>你好啊</div>
+      </>
     )
   }
 }
